7aef9d4cde46a2bf4d75d793c83727baa3b9ff89
MOHITUL <mohitulislam9678@gmail.com>
2025-10-08 23:11:07 +0600

    footer added

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -1,10 +1,62 @@
 import React from 'react';
+import { FaFacebookF, FaInfoCircle, FaInstagram, FaPaperclip, FaTwitch, FaTwitter } from 'react-icons/fa';
+import { FaTelegram } from 'react-icons/fa6';
+import { NavLink } from 'react-router';
 
 const Footer = () => {
     return (
-        <div>
-            
-        </div>
+        <footer className='bg-gradient-to-r from-[#632EE3] to-[#9F62F2] text-white py-6 mt-5 '>
+            <div className='container mx-auto px-4 shadow-xl'>
+                <div className='flex flex-col md:flex-row justify-between items-center text-center md:text-left'>
+                    <div className='mb-4 md:mb-0'>
+                        <h3 className='flex items-center justify-center md:justify-start text-xl font-semibold mb-2'>
+                            <FaPaperclip className='w-5 h-5 mr-2' /> Useful Sections
+                        </h3>
+
+                        <div className='space-x-4'>
+                            <NavLink to="/"> Home </NavLink>
+
+
+                            <NavLink to="/apps"> Apps </NavLink>
+
+                            <NavLink to="/installation"> Installation </NavLink>
+                        </div>
+                    </div>
+
+                    {/* about */}
+                    <div className='mb-4 md:mb-0'>
+                        <h3 className='flex items-center justify-center md:justify-start  text-xl font-semibold mb-2'> <FaInfoCircle className='w-5 h-5 mr-2' /> About Us </h3>
+                        <div className='space-x-4'>
+
+                            <NavLink> About Us </NavLink>
+
+                            <NavLink> Contact </NavLink>
+
+                            <NavLink> Report </NavLink>
+                        </div>
+                    </div>
+                </div>
+
+                {/* copyright */}
+                <div className='flex items-center my-4'>
+                    <hr className='flex-1 border-white' />
+                    <div className='mx-4 flex space-x-4'>
+                        <FaFacebookF className='w-5 h-5' />
+                        <FaInstagram className='w-5 h-5' />
+                        <FaTwitter className='w-5 h-5' />
+                        <FaTwitch className='w-5 h-5' />
+                        <FaTelegram className='w-5 h-5' />
+                    </div>
+                    <hr className='flex-1 border-white' />
+
+                </div>
+                <div className='text-center mt-4'>
+                    <p className='text-lg  text-gray-400'>
+                        © Copyright 2025 HERO.IO
+                    </p>
+                </div>
+            </div>
+        </footer>
     );
 };
 
